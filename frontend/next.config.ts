75366a2d358b26cf73076061c588f6c79846a52c
AmrajKoonar <amrajk31@gmail.com>
2025-06-09 17:02:34 -0700

    Fixed config options inside NextConfig

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true, // ✅ enables support for app/ routing
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
